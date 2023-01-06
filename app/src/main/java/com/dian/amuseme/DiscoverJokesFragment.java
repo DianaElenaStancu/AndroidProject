414c5a725542cdf137170b9e2f0f404102edc515
Bejan Andrei <bejan.andrei.official@gmail.com>
2023-01-06 15:15:58 +0200

    Designed the discover jokes page layout

--- app/src/main/java/com/dian/amuseme/DiscoverJokesFragment.java
+++ app/src/main/java/com/dian/amuseme/DiscoverJokesFragment.java
@@ -4,15 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class DiscoverJokesFragment extends Fragment {
+    private ImageButton imageButtonDiscardJoke, imageButtonSaveJokeToFavorites;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_discover_jokes, container, false);
+        View view = inflater.inflate(R.layout.fragment_discover_jokes, container, false);
+
+        setupViews(view);
+        connectComponents();
+
+        return view;
+    }
+
+    private void connectComponents() {
+        imageButtonDiscardJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Joke Discarded", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageButtonSaveJokeToFavorites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Joke Added To Favorites", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupViews(View view) {
+        imageButtonDiscardJoke = view.findViewById(R.id.imageButtonDiscardJoke);
+        imageButtonSaveJokeToFavorites = view.findViewById(R.id.imageButtonSaveJokeToFavorites);
     }
 }
