c663d68f641278ee9d5794d4e896c89a10188b31
Bejan Andrei <bejan.andrei.official@gmail.com>
2023-01-21 07:26:37 +0200

    implemented about us page

--- app/src/main/java/com/dian/amuseme/AboutUsFragment.java
+++ app/src/main/java/com/dian/amuseme/AboutUsFragment.java
@@ -7,12 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class AboutUsFragment extends Fragment {
+    ImageView imageViewDiana, imageViewAndrei;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+        setupViews(view);
+        return view;
+    }
+
+    private void setupViews(View view) {
+        imageViewDiana = view.findViewById(R.id.imageViewDiana);
+        imageViewAndrei = view.findViewById(R.id.imageViewAndrei);
+
+        Picasso.get()
+                .load("https://github.com/DianaElenaStancu.png")
+                .into(imageViewDiana);
+
+        Picasso.get()
+                .load("https://github.com/beji02.png")
+                .into(imageViewAndrei);
     }
 }
